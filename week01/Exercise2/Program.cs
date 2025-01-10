93a1e81c2d96cc531fc24ad80ab121a53aa15d27
bricehardy97 <har24047@byui.edu>
2025-01-09 22:07:53 -0700

    Added Grades to the percentages they received.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What grade did you receive?");
+        string answer=Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter ="";
+
+        if (grade >= 90)
+        {
+            letter ="A";
+        }
+        else if (grade >= 80)
+        {
+            letter= "B";
+        }
+        else if (grade >= 70)
+        {
+            letter ="C";
+        } 
+        else if (grade >= 70)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter ="F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine ("You Passed this course!");
+        }
+        else 
+        {
+            Console.WriteLine("Im sorry but you did not pass, please try again.");
+        }
     }
 }
