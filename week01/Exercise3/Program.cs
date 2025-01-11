ebf8d344d32feddd27a4baba535c4244755519e9
bricehardy97 <har24047@byui.edu>
2025-01-10 20:43:37 -0700

    Added in loops and if statements to create a number generator that you must guess.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number?");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber =randomGenerator.Next(1,100);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+        
+            if (magicNumber  > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Great Job you guessed the magic number!!");
+            }
+        }
     }
 }
