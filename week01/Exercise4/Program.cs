8a7f14edca5dd7c7717f1ef8269c0c91d6791c23
bricehardy97 <har24047@byui.edu>
2025-01-11 16:55:34 -0700

    Added in a list that can count the sums of each item, including the average and largest price amount.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> randomNumbers = new List<int>();
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write ("Enter any number or press 0 to quit : ");
+
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+
+            if (number!= 0)
+            {
+                randomNumbers.Add(number);
+            }
+        }
+        int sum =0;
+        foreach (int answer in randomNumbers)
+        {
+            sum += answer;
+        }
+        Console.WriteLine ($"The sum of your listed items is : {sum}");
+
+        float average = ((float)sum) / randomNumbers.Count;
+        Console.WriteLine($"The average of your listed items is {average}");
+
+        int largest = randomNumbers[0];
+
+        foreach (int answer in randomNumbers)
+        {
+            if (answer > largest)
+            {
+                largest = answer;
+            }
+        }
+        Console.WriteLine ($"The largest number in your list is : {largest}");
+
+
+
     }
 }
