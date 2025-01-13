7314aaefbf81ffbdbd0c7e83285b852703e724c0
bricehardy97 <har24047@byui.edu>
2025-01-12 22:24:14 -0700

    Added in functions to make A program that will square your favorite number.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName ();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayAnswer(userName, squareNumber);
+        
     }
-}
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favoirte number.");
+        int favorite = int.Parse(Console.ReadLine());
+        return favorite;
+    }
+    static int SquareNumber(int favorite)
+    {
+        int square = favorite * favorite;
+        return square;
+    }
+
+    static void DisplayAnswer(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
+       
+
+    
+    
+} 
