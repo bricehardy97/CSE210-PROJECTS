6b5c1efdf3bf321ddad84b41d3a7c49ba2aa211e
bricehardy97 <har24047@byui.edu>
2025-01-14 18:45:48 -0700

    working on jobs 1 and 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+       Job job1 = new Job() ;
+        job1._jobTitle = "Software Engineer" ;
+        job1._company = "Microsoft" ;
+        job1._startYear = 2019 ;
+        job1._endYear = 2022 ;
+
+       Job job2 = new Job() ;
+        job2._jobTitle = "Interpreter" ;
+        job2._company = "Purple" ;
+        job2._startYear = 2024 ;
+        job2._endYear = 2025 ;
+  
+  
+        Console.WriteLine("Job details: " + job1._company + ", " + myJob.Position);
+
     }
 }
