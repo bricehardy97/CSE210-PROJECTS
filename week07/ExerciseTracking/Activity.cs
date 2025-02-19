9ce4119a5f2ff93ddc8fea53317df1fe53b6e45a
bricehardy97 <har24047@byui.edu>
2025-02-18 19:41:59 -0700

    Created a code to track excercising workouts.

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,27 @@
+public abstract class Activity
+{
+   
+    private DateTime _date;
+    private int _minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+  
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+   
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+   
+    public string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {this.GetType().Name} ({Minutes} min) - Distance {GetDistance():0.0}, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+       
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 3.0),
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 30, 15.0),
+            new Swimming(new DateTime(2022, 11, 3), 30, 20)
+        };
+
+      
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
